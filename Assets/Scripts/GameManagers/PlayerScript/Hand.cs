0274ec3e8ff49eb4299b13064cb8e925d648d05f
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-02-17 20:05:09 -0500

    Init layout

---
+++ Assets/Scripts/GameManagers/PlayerScript/Hand.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hand")]
+public class NewBehaviourScript : ScriptableObject
+{
+    public List<Card> cards;
+}
