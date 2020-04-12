01aa8e9880d5de64d13c2318360ec49576f6152d
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-04-11 21:43:45 -0400

    Added basic hand UI

---
+++ Castle-Game-UProject/Assets/Scripts/UISystem/ClosablePanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClosablePanel : MonoBehaviour{
+
+    public RectTransform rt;
+    private Rect r;
+    
+    public float openPosition;
+    public float closedPosition;
+
+    public Button openToggle;
+    // Start is called before the first frame update
+    void Start(){
+        r = rt.rect;
+        openToggle.onClick.AddListener(closePanel);
+    }
+
+    public void closePanel(){
+        Debug.Log("Closing panel");     
+        openToggle.onClick.RemoveAllListeners();
+        openToggle.onClick.AddListener(openPanel);
+        rt.anchoredPosition = new Vector2(0, closedPosition);
+        openToggle.GetComponentInChildren<Text>().text = "Open";
+    }
+
+    public void openPanel(){
+        Debug.Log("Opening panel");    
+        openToggle.onClick.RemoveAllListeners();
+        openToggle.onClick.AddListener(closePanel);
+        rt.anchoredPosition = new Vector2(0, openPosition);
+        openToggle.GetComponentInChildren<Text>().text = "Close";
+    }
+    
+}
