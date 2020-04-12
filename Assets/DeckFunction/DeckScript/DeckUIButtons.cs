88250c0fb4ee3193f426f39bf548ef9d946d39e9
Kylist <quocanhhnams1316@gmail.com>
2020-04-11 22:07:32 -0400

    DeckFunction

---
+++ Assets/DeckFunction/DeckScript/DeckUIButtons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckUIButtons : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetScene()
+    {
+        // find all the cards and remove them
+        DeckUpdateSprite[] cards = FindObjectsOfType<DeckUpdateSprite>();
+        foreach (DeckUpdateSprite card in cards)
+        {
+            Destroy(card.gameObject);
+        }
+        ClearTopValues();
+        // deal new cards
+        FindObjectOfType<DeckFunction>().PlayCards();
+    }
+
+    void ClearTopValues()
+    {
+        DeckSelectable[] selectables = FindObjectsOfType<DeckSelectable>();
+        foreach (DeckSelectable selectable in selectables)
+        {
+            if (selectable.CompareTag("Top"))
+            {
+                selectable.suit = null;
+                selectable.value = 0;
+            }
+        }
+    }
+
+}
---
+++ Assets/DeckFunction/DeckScript/DeckUpdateSprite.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckUpdateSprite : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private DeckSelectable selectable;
+    private DeckFunction DeckFunct;
+    private DeckUserInput userInput;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> deck = DeckFunction.GenerateDeck();
+        DeckFunct = FindObjectOfType<DeckFunction>();
+        userInput = FindObjectOfType<DeckUserInput>();
+
+        int i = 0;
+        foreach (string card in deck)
+        {
+            if (this.name == card)
+            {
+                cardFace = DeckFunct.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<DeckSelectable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectable.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+
+        if (userInput.slot1)
+        {
+
+            if (name == userInput.slot1.name)
+            {
+                spriteRenderer.color = Color.yellow;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
+        }
+    }
+}
