e33c477167601985aa4fed83294da0094c6cf0eb
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-04-12 07:50:43 -0400

    Created radial health bar system and added it to unit UI test scene. Added pause menu system and started laying out system.

---
+++ Castle-Game-UProject/Assets/Scripts/UISystem/MenuPanelControllers/PauseMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour{
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            openPauseMenu();       
+        }
+    }
+
+    public void exitPauseMode(){
+        hidePanels();
+    }
+
+    public void hidePanels(){
+        pauseMenu.SetActive(false);    
+    }
+    
+    public void openPauseMenu(){
+        hidePanels();
+        pauseMenu.SetActive(true);
+    }
+}
