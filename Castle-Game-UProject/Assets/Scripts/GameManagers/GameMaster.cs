2b3c3dc707f8aed23cd824e80279a8c01e6e4bc4
Jonathan Goldenziel <tuh00941@temple.edu>
2020-04-12 01:37:34 -0400

    Added variable that keeps track of game state (turns, etc.)

--- Castle-Game-UProject/Assets/Scripts/GameManagers/GameMaster.cs
+++ Castle-Game-UProject/Assets/Scripts/GameManagers/GameMaster.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+
+    public enum GameState
+    {
+        CardPhase1, 
+        UnitPhase1, 
+        CardPhase2, 
+        UnitPhase2
+    }
+
+    public static GameState state;
+    
     void Awake(){
         // First thing we do is register all of the event types we will be using
         //EventInitializer.initEvents();
@@ -12,8 +23,7 @@ public class GameMaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        state = GameState.CardPhase1;
     }
 
     // Update is called once per frame
