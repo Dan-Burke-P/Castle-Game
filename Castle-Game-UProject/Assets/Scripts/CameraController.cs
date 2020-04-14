5f9216cf0d12747c8cb0ca7f9891047f6fd3770e
Daniel Burke <54857959+Dan-Burke-P@users.noreply.github.com>
2020-04-14 19:06:19 -0400

    Finished camera controller

--- Castle-Game-UProject/Assets/Scripts/CameraController.cs
+++ Castle-Game-UProject/Assets/Scripts/CameraController.cs
@@ -10,6 +10,17 @@ public class CameraController : MonoBehaviour{
     public Transform trns;
 
     /// <summary>
+    /// Arm that we will use to handle forward and backwards rotation
+    /// </summary>
+    public Transform rotationArm;
+
+    /// <summary>
+    /// Scalar for our rotation that will make it easier to fine tune in the options menu
+    /// </summary>
+    [SerializeField]
+    private float _rotScaler;
+    
+    /// <summary>
     /// How fast do we want translation to occur
     /// </summary>
     public float translateSpeed;
@@ -33,6 +44,15 @@ public class CameraController : MonoBehaviour{
     /// Defines how far the camera can move vertically until it hits the border
     /// </summary>
     public Vector2 vertBounds;
+
+    /// <summary>
+    /// Clamping for the forwards and backwards rotation
+    /// </summary>
+    public Vector2 rotBounds;
+
+
+    public float yAxisRotation = 0f;
+    
     // Start is called before the first frame update
     void Start(){
         trns = gameObject.GetComponent<Transform>();
@@ -48,13 +68,21 @@ public class CameraController : MonoBehaviour{
         if (Input.mousePosition.x >= Screen.width - edgeDistance){
             // Right hand movement
             Vector3 targetPos = trns.position + trns.right * translateSpeed;
-            if (targetPos.x > horzBounds.x && targetPos.x < horzBounds.y){
+            if (targetPos.x > horzBounds.x 
+                && targetPos.x < horzBounds.y
+                && targetPos.z > vertBounds.x
+                && targetPos.z < vertBounds.y){
+                
                 trns.position = targetPos;
             }
-        }else if (Input.mousePosition.x <= edgeDistance && trns.position.x > horzBounds.x){
+        }else if (Input.mousePosition.x <= edgeDistance){
             // Lefthand movement
             Vector3 targetPos = trns.position - trns.right * translateSpeed;
-            if (targetPos.x > horzBounds.x && targetPos.x < horzBounds.y){
+            if (   targetPos.x > horzBounds.x 
+                && targetPos.x < horzBounds.y
+                && targetPos.z > vertBounds.x
+                && targetPos.z < vertBounds.y){
+                
                 trns.position = targetPos;
             }
         }
@@ -63,21 +91,54 @@ public class CameraController : MonoBehaviour{
             // Forward movement
             Vector3 targetPos = trns.position + trns.forward * translateSpeed;
 
-            if (targetPos.z > vertBounds.x && targetPos.z < vertBounds.y){
+            if (   targetPos.x > horzBounds.x 
+                && targetPos.x < horzBounds.y
+                && targetPos.z > vertBounds.x
+                && targetPos.z < vertBounds.y){
+                
                 trns.position = targetPos;
             }
-        }else if (Input.mousePosition.y <= edgeDistance && trns.position.z > vertBounds.x){
+        }else if (Input.mousePosition.y <= edgeDistance){
             // Backward movement
             Vector3 targetPos = trns.position - trns.forward * translateSpeed;
 
-            if (targetPos.z > vertBounds.x && targetPos.z < vertBounds.y){
+            if (   targetPos.x > horzBounds.x 
+                && targetPos.x < horzBounds.y
+                && targetPos.z > vertBounds.x
+                && targetPos.z < vertBounds.y){
+                
                 trns.position = targetPos;
             }
         }
     }
 
     public void checkRotation(){
+        if (Input.GetKey(KeyCode.UpArrow)){
+            // Rotate forward
+            float rotationAmount = yAxisRotation + (rotationSpeed * _rotScaler);
+            if (rotationAmount < rotBounds.y){
+                rotationArm.Rotate(rotationSpeed * _rotScaler,0,0);
+                yAxisRotation += (rotationSpeed * _rotScaler);
+
+            }
+        }else if(Input.GetKey(KeyCode.DownArrow)){
+            // Rotate backwards
+            float rotationAmount = yAxisRotation - (rotationSpeed * _rotScaler);
+            if (rotationAmount > rotBounds.x){
+                rotationArm.Rotate(-rotationSpeed * _rotScaler,0,0);
+                yAxisRotation -= (rotationSpeed * _rotScaler);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            // Rotate to the left
+            trns.Rotate(0,rotationSpeed * _rotScaler,0);
+        }else if (Input.GetKey(KeyCode.RightArrow)){
+            // Rotate to the right
+            trns.Rotate(0,-rotationSpeed * _rotScaler,0);
+        }
         
+        //trns.rotation.eulerAngles.Set(0,trns.rotation.eulerAngles.y,0);
     }
     
 }
