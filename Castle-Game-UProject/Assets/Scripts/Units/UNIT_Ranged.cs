4c4b35a8cd64f02086e2900aaa4bc15c554d3a35
Phil Nyce <tug81999@temple.edu>
2020-04-19 19:30:32 -0400

    Unit Updates
    1. Added unit task version of attack function, and added it to the list.
    2. Implemented onDeath function: checks for currHP <= 0, destroys game object if true.
    3. Added onDeath function to HandleAttack in appropriate places.
    4. Cleaned up HandleAttack a bit.
    5. Added check for unit owner to attack(BaseUnit) function, now returns early if the ownerID of the caller and the argument are the same.
    6. Added attackCost field to BaseUnit. Attacks can now only be made when in RNG squares of the target. All attacks decrement currAP by attackCost.
    7. Changed AP cost of movement to zero in the move unit task, as AP will be deducted based on distance travelled (I have NOT yet added pathing however).

--- Castle-Game-UProject/Assets/Scripts/Units/UNIT_Ranged.cs
+++ Castle-Game-UProject/Assets/Scripts/Units/UNIT_Ranged.cs
@@ -8,19 +8,8 @@ public class UNIT_Ranged : BaseUnit {
 	public UNIT_Ranged() {
 		unitType = UnitType.Ranged;
 		unitName = "Ranged";
-		RNG = 2;
+		RNG = 3;
 		DEF = 20;
 		vsSoldier = 1.5f;
-	}
-	
-	public override void attack(BaseUnit defender) {
-		int dist = System.Math.Abs(defender.xPos - this.xPos) + System.Math.Abs(defender.yPos - this.yPos);
-		if(dist <= RNG) {
-			AttackHandler.Instance().attackEvent.raise(0, this, new Dictionary<string, object> {
-											{"Attacker", this},
-											{"Defender", defender}
-											});
-			this.currAP--;
-		}
 	}
 }
